3aea72fe89afe3dff18333ec8cf4af239106efd2
Alexander Rowland <arowland900@gmail.com>
2020-09-29 14:36:10 -0700

    install mongoose, dotenv, add database.js file

---
+++ config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/lookbook', { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.once('connected', () => {
+  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+});
