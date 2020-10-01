378a140260b2a7c9ce9b91f68025b6f13518edb2
Alexander Rowland <arowland900@gmail.com>
2020-10-01 13:15:42 -0700

    add fade in for item detail page

--- src/pages/ItemDetailPage/ItemDetailPage.jsx
+++ src/pages/ItemDetailPage/ItemDetailPage.jsx
@@ -1,30 +1,54 @@
-import React from 'react';
-import {  Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import $ from 'jquery';
 import './ItemDetailPage.css';
 
-const ItemDetailPage = (props) => {
-    console.log("PROPS: ", props)
-  const item = props.location.state.item
-  console.log(item)
+class ItemDetailPage extends Component {
+  // item = this.props.location.state.item
+
+  state = {
+    item:''
+  }
   
-  return (
-    <div className='ItemDetailPage'>
-      {item ?
-        <div className='ItemDetailPage-starship'>
-          <span>NAME:</span>
-          <span>{item.name}</span>
-          <span>DESCRIPTION:</span>
-          <span>{item.description}</span>
-          <span>
-            <img src={item.photos[0]} alt=""/>
-          </span>
-          <Link to='/'>RETURN</Link>
-        </div>
-        :
-        <h3>Loading...</h3>
-      }
-    </div>
-  );
+
+  fadeDiv = () => {
+    let div = document.querySelector('.itemDetailPage')
+    // setTimeout(function(){
+    $(div).fadeIn('slow')
+
+    // }, 500)
+
+
+  }
+
+  /*--- Lifecycle Methods ---*/
+
+  componentDidMount = async () => {
+    let item = await this.props.location.state.item
+    this.setState({ item })
+    await this.fadeDiv()
+
+  }
+  render() {
+    return (
+      <div className='itemDetailPage'>
+        {this.state.item ?
+          <div className='ItemDetailPage-item'>
+            <span>NAME:</span>
+            <span>{this.state.item.name}</span>
+            <span>DESCRIPTION:</span>
+            <span>{this.state.item.description}</span>
+            <span>
+              <img src={this.state.item.photos[0]} alt="" />
+            </span>
+            <Link to='/'>RETURN</Link>
+          </div>
+          :
+          <h3>Loading...</h3>
+        }
+      </div>
+    );
+  }
 };
 
 export default ItemDetailPage;
